62e58fed4d9dc0f8c094c6ba87cb9cf66c1d14d0
soowan <nick6767676767@gmail.com>
2025-07-25 01:13:12 +0900

    feat: 3차 디자인 수정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"], 
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     container: {
